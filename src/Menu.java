9e51be84828d59d6fc98d04e81126b850f107a34
Matias Torres <matiastowers123@gmail.com>
2026-01-29 17:10:20 -0300

    Primer prototipo del menu

---
+++ src/Menu.java
@@ -0,0 +1,18 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Menu {
+    static void main(String[] args) {
+        System.out.println("Sea Bienvenido al Conversor de monedas");
+
+        System.out.println("1) Dolar => Peso argentino");
+        System.out.println("2) Peso argentino => Dolar");
+        System.out.println("3) Dolar => Real brasileño");
+        System.out.println("4) Real brasileño => Dolar");
+        System.out.println("5) Salir");
+        System.out.println("Elija una opcion valida:");
+        Scanner lectura = new Scanner(System.in);
+
+
+    }
+}
